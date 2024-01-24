9d19be3f6f8e21b62fc5f2a8d22d80980f78f363
JamesSleep <supercurve2256@gmail.com>
2024-01-25 03:35:45 +0900

    메인페이지 작업전 세팅

--- src/pages/Ranking.tsx
+++ src/pages/Ranking.tsx
@@ -1,13 +1,45 @@
-import { Pressable, Text, View } from 'react-native';
+import { useCallback, useState } from 'react';
+import { Alert, Button, Pressable, Text, View } from 'react-native';
+import { Rating } from 'react-native-ratings';
+import YoutubePlayer, { PLAYER_STATES } from 'react-native-youtube-iframe';
 import { MainDrawScreenProps } from 'src/navigations/MainDrawNavigation';
+import Icon from 'react-native-vector-icons/FontAwesome5';
 
 function Ranking({ navigation }: MainDrawScreenProps<'Ranking'>) {
+  const [playing, setPlaying] = useState(false);
+
+  const onStateChange = useCallback((state: PLAYER_STATES) => {
+    if (state === 'ended') {
+      setPlaying(false);
+      Alert.alert('video has finished playing!');
+    }
+  }, []);
+
+  const togglePlaying = useCallback(() => {
+    setPlaying(prev => !prev);
+  }, []);
+
   return (
     <View>
       <Text>Ranking</Text>
       <Pressable onPress={() => navigation.navigate('Match')}>
         <Text style={{ color: 'black' }}>Match</Text>
       </Pressable>
+      <YoutubePlayer
+        height={300}
+        play={playing}
+        videoId={'b56apBhNYkM'}
+        onChangeState={onStateChange}
+      />
+      <Button title={playing ? 'pause' : 'play'} onPress={togglePlaying} />
+      <Rating
+        type="heart"
+        ratingCount={3}
+        imageSize={60}
+        showRating
+        //onFinishRating={this.ratingCompleted}
+      />
+      <Icon name="comments" size={20} color="#900" />
     </View>
   );
 }
